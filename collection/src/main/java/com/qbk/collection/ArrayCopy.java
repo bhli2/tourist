776e092682d456e879ad57a7e1813d2cdb9875db
quboka <quboka@telecomyt.com.cn>
2019-10-31 17:32:16 +0800

    数组复制

--- collection/src/main/java/com/qbk/collection/ArrayCopy.java
+++ collection/src/main/java/com/qbk/collection/ArrayCopy.java
@@ -1,13 +1,34 @@
 package com.qbk.collection;
 
+import java.util.Arrays;
+
 /**
  * 数组复制
- * TODO
  */
 public class ArrayCopy {
 
     public static void main(String[] args) {
-        //System.arraycopy();
-
+        /**
+         *
+         * * @param      src      源数组
+         * * @param      srcPos   源数组要复制的起始位置
+         * * @param      dest     目标数组
+         * * @param      destPos  目标数组复制的起始位置
+         * * @param      length   复制的长度
+         *  public static native void arraycopy(Object src,  int  srcPos,
+         *                                         Object dest, int destPos,
+         *                                         int length);
+         */
+        //第一个源数组
+       char[] src1 = {'a','b','c'};
+        //第二个源数组
+       char[] src2 = {'d','e'};
+       //首先把第一个数组复制进目标数组 且数组长度为两个数组长度之和
+       char[] dest = Arrays.copyOf(src1, src1.length + src2.length);
+       System.out.println(dest);
+       System.out.println(dest.length);
+       //然后把第二个数组复制进目标数组，目标数组复制起始位置为第一个数组长度，复制长度为第二个数组长度
+       System.arraycopy(src2,0,dest,src1.length,src2.length);
+       System.out.println(dest);
     }
 }
