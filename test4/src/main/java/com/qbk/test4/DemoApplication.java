85421889d2797e2a7734fe299d22e76f341268ff
quboka <quboka@telecomyt.com.cn>
2019-11-21 20:54:44 +0800

    MockMvc

--- test4/src/main/java/com/qbk/test4/DemoApplication.java
+++ test4/src/main/java/com/qbk/test4/DemoApplication.java
@@ -2,9 +2,24 @@ package com.qbk.test4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+
+    @GetMapping("/get")
+    public Map get(int id ,String name){
+        System.out.println("------------------"+id);
+        System.out.println("------------------"+name);
+        HashMap<String,String> hashMap = new HashMap<>();
+        hashMap.put("code","200");
+        return hashMap;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
---
+++ test4/src/test/java/com/qbk/test4/MockTest.java
@@ -0,0 +1,71 @@
+package com.qbk.test4;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @RunWith(SpringRunner.class)， 就是指用SpringRunner来运行，其中 SpringJUnit4ClassRunner 和 SpringRunner 区别是什么？
+ * SpringRunner是SpringJUnit4ClassRunner的别名
+ *
+ * @SpringBootTest 是SpringBoot的一个用于测试的注解，通过SpringApplication在测试中创建ApplicationContext。
+ *
+ * @AutoConfigureMockMvc 是用于自动配置MockMvc
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MockTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    /**
+     * 无须启动项目 可直接调用接口
+     */
+    @Test
+    public void TestXXX() throws Exception {
+        /**
+         * .perform() 执行一个MockMvcRequestBuilders请求。其中.get()表示发送get请求（可以使用get、post、put、delete等）；
+         * .contentType()设置编码格式；.param()请求参数,可以带多个。
+         *   andExpect()添加 MockMvcResultMatchers验证规则，验证执行结果是否正确。
+         *   .andDo()添加 MockMvcResultHandlers结果处理器,这是可以用于打印结果输出。
+         *   .andReturn()结果还回，然后可以进行下一步的处理。
+         */
+        MvcResult result = mockMvc.perform(
+                MockMvcRequestBuilders.get("/get")
+                        .param("id", "15")
+                        .param("name","kk")
+                        .header("Authorization", "Bearer c3af4cf0-58c5-4953-aba5-e40e8c901a51")
+                )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+        System.out.println(result);
+        System.out.println("----------------");
+        //返回值
+        System.out.println(result.getResponse().getContentAsString());
+        System.out.println("----------------");
+        //状态
+        System.out.println(result.getResponse().getStatus());
+    }
+
+
+
+
+}
