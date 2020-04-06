1f56f04105db92b6548fb5ebcf3ae669cb322aa7
quboka <1362420246@qq.com>
2020-04-06 12:06:53 +0800

    amqp

--- mq/spring-boot-amqp-demo/src/main/java/com/qbk/mq/amqp/demo/provider/MyProvider.java
+++ mq/spring-boot-amqp-demo/src/main/java/com/qbk/mq/amqp/demo/provider/MyProvider.java
@@ -12,21 +12,23 @@ public class MyProvider {
 
     public void send(){
 
-        // Default 默认交换机,默认交换机其实就是直连交换机,可以理解为名称为空字符串的直连交换机,
-        // 一个queue若不指定binding的交换机,就被绑定到默认交换机上,routingKey为queue的名称
-        amqpTemplate.convertAndSend("","FIRST_QUEUE","-------- a direct msg");
-
         /**
          * @param exchange 交换机的名字
          * @param routingKey  路由key
          * @param message 发送的消息
          */
+        // Default 默认交换机,默认交换机其实就是直连交换机,可以理解为名称为空字符串的直连交换机,
+        // 一个queue若不指定binding的交换机,就被绑定到默认交换机上,routingKey为queue的名称
+        amqpTemplate.convertAndSend("","FIRST_QUEUE","-------- a direct msg");
+
+        //主题交换机  routingKey可以用通配符规则路由
         amqpTemplate.convertAndSend("TOPIC_EXCHANGE","shanghai.qbk.travel","-------- a topic msg : shanghai.qbk.消息");
         amqpTemplate.convertAndSend("TOPIC_EXCHANGE","beijing.qbk.work","-------- a topic msg : beijing.qbk.消息");
 
+        //广播交换机  没有routingKey
         amqpTemplate.convertAndSend("FANOUT_EXCHANGE","","-------- a fanout msg");
 
+        //直连交换机 精准匹配routingKey
         amqpTemplate.convertAndSend("myDirectExchange","mine.direct","this is a direct message");
     }
-
 }
