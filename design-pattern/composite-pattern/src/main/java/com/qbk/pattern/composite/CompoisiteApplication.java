b771de1e26541d15bb0250bb5e70d621b8c66feb
quboka <quboka@telecomyt.com.cn>
2020-05-18 10:53:36 +0800

    main函数

---
+++ design-pattern/composite-pattern/src/main/java/com/qbk/pattern/composite/CompoisiteApplication.java
@@ -0,0 +1,11 @@
+package com.qbk.pattern.composite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompoisiteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CompoisiteApplication.class,args);
+    }
+}
---
+++ design-pattern/flyweight-pattern/src/main/java/com/qbk/pattetrn/flyweight/FlyweightPatternApplication.java
@@ -0,0 +1,11 @@
+package com.qbk.pattetrn.flyweight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyweightPatternApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlyweightPatternApplication.class,args);
+    }
+}
