bb907ff22e9c39c1e36bac122ba17df6891c1330
quboka <1362420246@qq.com>
2020-05-18 23:56:05 +0800

    锁的中断

---
+++ juc/src/main/java/com/qbk/lock/lockInterruptiblyDemo.java
@@ -0,0 +1,71 @@
+package com.qbk.lock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 锁可中断
+ * lockInterruptibly()
+ */
+public class lockInterruptiblyDemo {
+
+    public static ReentrantLock lock1 = new ReentrantLock();
+    public static ReentrantLock lock2 = new ReentrantLock(true);
+
+    public static void main(String[] args) throws InterruptedException {
+
+        final Thread thread1 = new Thread(() -> {
+            try {
+                lock1.lockInterruptibly();
+
+                Thread.sleep(1000);
+
+                lock2.lockInterruptibly();
+                System.out.println(Thread.currentThread().getName() + "，完成。");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.out.println(Thread.currentThread().getName() + "，失败。");
+            }finally {
+                // 查询当前线程是否保持此锁。
+                if (lock1.isHeldByCurrentThread()) {
+                    lock1.unlock();
+                }
+                if (lock2.isHeldByCurrentThread()) {
+                    lock2.unlock();
+                }
+                System.out.println(Thread.currentThread().getName() + "，退出。");
+            }
+        }, "thread1");
+
+        final Thread thread2 = new Thread(() -> {
+            try {
+                lock2.lockInterruptibly();
+
+                Thread.sleep(1000);
+
+                lock1.lockInterruptibly();
+                System.out.println(Thread.currentThread().getName() + "，完成。");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.out.println(Thread.currentThread().getName() + "，失败。");
+            }finally {
+                if (lock2.isHeldByCurrentThread()) {
+                    lock2.unlock();
+                }
+                if (lock1.isHeldByCurrentThread()) {
+                    lock1.unlock();
+                }
+                System.out.println(Thread.currentThread().getName() + "，退出。");
+            }
+        },"thread2");
+
+
+        thread1.start();
+        thread2.start();
+
+        Thread.sleep(3000);
+        thread2.interrupt(); // 中断线程2
+
+        Thread.sleep(1000);
+        System.out.println("结束");
+    }
+}
