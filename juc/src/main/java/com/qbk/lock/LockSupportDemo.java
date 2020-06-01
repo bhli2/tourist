926337314c42c0c09858b5ae139e7932f55f5f65
quboka <1362420246@qq.com>
2020-06-02 00:08:46 +0800

    LockSupport

---
+++ juc/src/main/java/com/qbk/lock/LockSupportDemo.java
@@ -0,0 +1,26 @@
+package com.qbk.lock;
+
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ *  阻塞/唤醒
+ *  LockSupport.park();
+ *  LockSupport.unpark();
+ */
+public class LockSupportDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(() -> {
+            System.out.println("睡觉");
+            LockSupport.park();
+            System.out.println("起床");
+            System.out.println("是否中断：" + Thread.currentThread().isInterrupted());
+        });
+        t.setName("A-Name");
+        t.start();
+        Thread.sleep(3000);
+        System.out.println("妈妈喊我起床");
+        //t.interrupt();
+        LockSupport.unpark(t);
+    }
+}
