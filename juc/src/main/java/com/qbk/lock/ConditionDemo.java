46fa9d3f1ce91faf1054039e31115e98f2257d26
quboka <1362420246@qq.com>
2020-06-07 15:41:55 +0800

    Condition

---
+++ juc/src/main/java/com/qbk/lock/ConditionDemo.java
@@ -0,0 +1,46 @@
+package com.qbk.lock;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Condition
+ */
+public class ConditionDemo {
+
+    private static Lock lock = new ReentrantLock();
+    private static Condition condition = lock.newCondition();
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(
+                ()->{
+                    System.out.println("线程阻塞");
+                    lock.lock();
+                    try {
+                        condition.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }finally {
+                        lock.unlock();
+                    }
+                    System.out.println("线程结束");
+                }
+        );
+        Thread t2 = new Thread(
+                ()->{
+                    lock.lock();
+                    try {
+                        System.out.println("线程唤醒");
+                        condition.signal();
+                    } finally {
+                        lock.unlock();
+                    }
+                }
+        );
+        t1.start();
+        TimeUnit.SECONDS.sleep(3);
+        t2.start();
+    }
+}
