b0b471dfd9d46b7b5fcccf06944babc6a9c88afe
quboka <1362420246@qq.com>
2020-07-30 15:45:51 +0800

    jdk9

---
+++ javademo/jdk9demo/src/main/java/com/qbk/jdk9demo/Jdk9DemoApplication.java
@@ -0,0 +1,11 @@
+package com.qbk.jdk9demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdk9DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Jdk9DemoApplication.class,args);
+    }
+}
