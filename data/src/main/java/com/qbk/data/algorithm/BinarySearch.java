5d9fa7c7085679b2432d7ecc0aac3ab24b0e7868
quboka <1362420246@qq.com>
2020-08-03 15:34:09 +0800

    二分法递归的方式

--- data/src/main/java/com/qbk/data/algorithm/BinarySearch.java
+++ data/src/main/java/com/qbk/data/algorithm/BinarySearch.java
@@ -8,11 +8,15 @@ import java.util.Arrays;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {677,265,222,25,111,8,13,33,0,87,25,67,55};
+
         arr = sort(arr);
         System.out.println(Arrays.toString(arr));
 
-        int i = find(arr , 677);
+        int i = find(arr , 222);
         System.out.println(i);
+
+        int j = find(arr , 221,0 ,arr.length-1);
+        System.out.println(j);
 
     }
 
@@ -38,7 +42,7 @@ public class BinarySearch {
     }
 
     /**
-     * 查找 位置
+     * 循环的方式 查找 位置
      */
     private static int find(int[] arr ,int n){
         int start =  0 ;
@@ -59,5 +63,24 @@ public class BinarySearch {
             middle = (start + end) / 2 ;
         }
         return -1;
+    }
+
+    /**
+     * 递归的方式 查找 位置
+     */
+    private static int find(int[] arr ,int n , int start , int end ){
+        System.out.println( start  + ":" + end);
+        if(end < start){
+            return -1;
+        }
+        int middle =  (start + end) / 2 ;
+        if( n == arr[middle]){
+            return middle;
+        }else if(n > arr[middle]){
+            start = middle + 1;
+        }else if(n < arr[middle]){
+            end = middle -1 ;
+        }
+        return find(arr,n,start,end);
     }
 }
