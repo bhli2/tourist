bb720b5ffbfd3583e7b0f6791211dfcbf104b8c3
quboka <1362420246@qq.com>
2020-09-09 16:19:12 +0800

    hbase客户端两种封装

---
+++ nosql/hbase-demo/src/test/java/com/qbk/nosql/hbase/demo/HBaseUtilTest.java
@@ -0,0 +1,67 @@
+package com.qbk.nosql.hbase.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * HBaseUtil 测试
+ */
+@SpringBootTest(classes = HbaseDemoApplication.class)
+public class HBaseUtilTest {
+
+    /**
+     * put data test
+     */
+    @Test
+    public void testPut() {
+
+    }
+
+    /**
+     * delete data test
+     */
+    @Test
+    public void testDelete() {
+
+    }
+
+    /**
+     * get data test
+     */
+    @Test
+    public void testGet() {
+
+    }
+
+    /**
+     * scan data test
+     */
+    @Test
+    public void testScan() {
+
+    }
+
+    /**
+     * scan filter data test
+     */
+    @Test
+    public void testScanFilter() {
+
+    }
+
+    /**
+     * get data test
+     */
+    @Test
+    public void getTest() {
+
+    }
+
+    /**
+     * scan data test
+     */
+    @Test
+    public void scanTest(){
+
+    }
+}
