bb219c4882b37fc5c6be902bf4b46b1052f60eec
quboka <1362420246@qq.com>
2020-10-28 18:46:39 +0800

    spring读取外部配置

---
+++ spring-demo/spring-boot-external/src/main/java/com/qbk/spring/demo/external/SpringExternalApplication.java
@@ -0,0 +1,11 @@
+package com.qbk.spring.demo.external;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExternalApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExternalApplication.class,args);
+    }
+}
---
+++ spring-demo/spring-boot-external/src/main/java/com/qbk/spring/demo/external/controller/ExternalController.java
@@ -0,0 +1,18 @@
+package com.qbk.spring.demo.external.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExternalController {
+
+    @Value("${info.name}")
+    private String str;
+
+    @RequestMapping("/get")
+    public String get(){
+        return str;
+    }
+}
