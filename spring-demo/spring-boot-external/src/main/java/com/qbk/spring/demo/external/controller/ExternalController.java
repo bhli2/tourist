8b2a6709e0573ef0a798f3368ead122c44f2f59e
quboka <1362420246@qq.com>
2020-10-29 14:39:51 +0800

    jar包瘦身

--- spring-demo/spring-boot-external/src/main/java/com/qbk/spring/demo/external/controller/ExternalController.java
+++ spring-demo/spring-boot-external/src/main/java/com/qbk/spring/demo/external/controller/ExternalController.java
@@ -1,6 +1,5 @@
 package com.qbk.spring.demo.external.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
