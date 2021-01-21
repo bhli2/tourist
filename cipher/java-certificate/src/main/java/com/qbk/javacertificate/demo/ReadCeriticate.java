476a84149b8214f0788a6d0865d296d884cfeef5
quboka <1362420246@qq.com>
2021-01-21 14:30:40 +0800

    注释

--- cipher/java-certificate/src/main/java/com/qbk/javacertificate/demo/ReadCeriticate.java
+++ cipher/java-certificate/src/main/java/com/qbk/javacertificate/demo/ReadCeriticate.java
@@ -21,14 +21,16 @@ public class ReadCeriticate {
     public static void main(String[] args) throws Exception{
         //临时bc方法添加都环境变量
         Security.addProvider(new BouncyCastleProvider());
-
+        //读取证书
         FileInputStream fileInputStream = new FileInputStream("C:\\Users\\DXYT\\Desktop\\certificate\\qbk.cer");
-
+        //获取证书
         CertificateFactory certificateFactory = CertificateFactory.getInstance("X.509", "BC");
         Certificate certificate = certificateFactory.generateCertificate(fileInputStream);
         X509Certificate x509Certificate = (X509Certificate)certificate;
+        //获取公钥
         PublicKey publicKey = x509Certificate.getPublicKey();
 
+        //获取 扩展信息
         ASN1ObjectIdentifier asn1ObjectIdentifier = new ASN1ObjectIdentifier("1.2.3.4");
         byte[] extensionValue = x509Certificate.getExtensionValue(asn1ObjectIdentifier.getId());
 
@@ -37,10 +39,11 @@ public class ReadCeriticate {
 
         String ciphertext = new String(octetString, StandardCharsets.UTF_8);
 
+        byte[] decode = Base64.getDecoder().decode(ciphertext);
+
+        //使用公钥 给扩展信息解密
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.DECRYPT_MODE, publicKey);
-
-        byte[] decode = Base64.getDecoder().decode(ciphertext);
         byte[] bytes = cipher.doFinal(decode);
         String result = new String(bytes, StandardCharsets.UTF_8);
         System.out.println(result);
